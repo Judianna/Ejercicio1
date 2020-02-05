f98417bdbec86b2f4738511c28339c673021057c
Judianna <58674281+Judianna@users.noreply.github.com>
2020-02-04 23:46:17 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -19,6 +19,9 @@ namespace ejer1
             float semanas= dias/7;
             Console.WriteLine("La cantidad de días son {0}",dias);
             Console.WriteLine("la cantidad de semanas {0}",semanas);
+            Console.WriteLine("La fecha 1 es: {0}", f1.ToString("dddd, d 'de' MMMM 'de' yyyy"));
+        
+
         }
     }
 }
